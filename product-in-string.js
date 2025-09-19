a6dec0df808634c1e6dfea9664c2a05dd039b6e2
Jaguar-H <himanshujaguri433@gmail.com>
2025-09-19 17:54:58 +0530

    create multiplication with strings

---
+++ product-in-string.js
@@ -0,0 +1,16 @@
+let stringOne = "........"
+let stringTwo = "..."
+let product = ""
+
+const limit = stringTwo.length 
+
+for(let i = 0 ; i < limit ; i++){
+    product = product + stringOne
+    console.log(product);
+    
+}
+
+console.log("the product is :", product.length,"\n",product;
+
+
+
