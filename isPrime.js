b2d3ab870059a13c08eab43c032fa5e347d39a08
Jaguar-H <himanshujaguri433@gmail.com>
2025-09-19 14:14:03 +0530

    uploaded the correct code

--- isPrime.js
+++ isPrime.js
@@ -1,12 +1,19 @@
-const numbertocheck = 23;
+const numbertocheck = 0;
 let isPrime = false 
 let suffix = ""
 
-for(let x = 2 ; x <= (numbertocheck / 2) ; x++){
+if (numbertocheck <= 1){
+    console.log("Can't check ",numbertocheck,"for prime");
+    
+}
+else{
+   for(let x = 2 ; x <= (numbertocheck / 2) ; x++){
     if (numbertocheck % x !== 0){
         isPrime = true
     }
+} 
 }
+
 suffix = isPrime ? "is Prime" : "Not Prime "
 
 console.log(numbertocheck , suffix );
