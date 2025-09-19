0eb036de57baf2fbe9bf6ae4ebd00469422a2046
Jaguar-H <himanshujaguri433@gmail.com>
2025-09-19 14:01:48 +0530

    code to practice the ternary operator
    this code takes id and password as input and uses ternary  operation to check  whether to give login message or failed message

---
+++ ternary.js
@@ -0,0 +1,9 @@
+let id =123
+let password=1234
+let login;
+let request  = 0
+login=(id===123)? password===1234:false;
+
+request=(login===true)? "login Successful for user 1":"login Failed try again ";
+
+console.log(request);
