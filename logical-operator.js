37073ee00cad95b9ac7c395185045d63175d7239
Jaguar-H <himanshujaguri433@gmail.com>
2025-09-19 14:03:46 +0530

    code to practice the logical operator

---
+++ logical-operator.js
@@ -0,0 +1,9 @@
+let input1 = true 
+let input2 = false  
+if (input1 === true || (input2 === true)) {
+    console.log("OR True");    
+}
+if (input1 === true && input2 === true) {
+    console.log("AND TRUE "); 
+}
+console.log("NOT for a ",!input1," NOT for b ",!input2);
