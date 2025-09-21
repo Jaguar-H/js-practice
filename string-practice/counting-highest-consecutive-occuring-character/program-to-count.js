eb0752314f98b2d0cd4bacea5632d5758a22d5a0
Jaguar-H <himanshujaguri433@gmail.com>
2025-09-21 21:39:10 +0530

    Update program-to-count  to show which characters are repeated

--- string-practice/counting-highest-consecutive-occuring-character/program-to-count.js
+++ string-practice/counting-highest-consecutive-occuring-character/program-to-count.js
@@ -5,20 +5,35 @@ let previousLetter = ""
 let repetedString = ""
 let   counter = 0 
 let highestCount  = 0 
+let repeatingString = ""
+let highestRepeatingString = ""
 
 for(let letter = 0 ; letter < limit ; letter ++  ){
     currentLetter = input[letter]
 
     if (currentLetter !== previousLetter){
+
         highestCount = counter > highestCount ? counter : highestCount
+        highestRepeatingString = counter > highestCount ? repeatingString : highestRepeatingString
+        
         counter = (counter * 0 ) + 1
+        repeatingString = ""
+        repeatingString += currentLetter
         previousLetter = currentLetter
+
     }else{
+
         counter += 1
-        highestCount = counter > highestCount ? counter : highestCount
+        repeatingString += currentLetter
         previousLetter = currentLetter
+        
+        highestRepeatingString = counter > highestCount ? repeatingString : highestRepeatingString
+        highestCount = counter > highestCount ? counter : highestCount
+
     }
 
 }
-console.log("the highest count of a string repeating consequtively is :",highestCount);
+console.log("the highest count of a string repeating consequtively is :",highestCount )
+console.log("And the string is :", highestRepeatingString);
+
 
